4a84eed6c46a4288a5b983e04153f8af70392709
NDMINH-dev <55449378+NDMINH-dev@users.noreply.github.com>
2020-05-31 23:04:56 +0700

    Add files via upload
    +Giáo viên, Nhân viên (thêm,xóa,sửa)

---
+++ Controllers/GIAOVIENsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebKhoaHocUngDung.Models;
+
+namespace WebKhoaHocUngDung.Areas.Admin.Controllers
+{
+    public class GIAOVIENsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/GIAOVIENs
+        public ActionResult Index()
+        {
+            return View(db.GIAOVIENs.ToList());
+        }
+
+        // GET: Admin/GIAOVIENs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GIAOVIEN gIAOVIEN = db.GIAOVIENs.Find(id);
+            if (gIAOVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gIAOVIEN);
+        }
+
+        // GET: Admin/GIAOVIENs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/GIAOVIENs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaGV,HoTenGV,SDTGV,DiaChi,Phai,NgaySinh")] GIAOVIEN gIAOVIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GIAOVIENs.Add(gIAOVIEN);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(gIAOVIEN);
+        }
+
+        // GET: Admin/GIAOVIENs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GIAOVIEN gIAOVIEN = db.GIAOVIENs.Find(id);
+            if (gIAOVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gIAOVIEN);
+        }
+
+        // POST: Admin/GIAOVIENs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaGV,HoTenGV,SDTGV,DiaChi,Phai,NgaySinh")] GIAOVIEN gIAOVIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gIAOVIEN).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(gIAOVIEN);
+        }
+
+        // GET: Admin/GIAOVIENs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GIAOVIEN gIAOVIEN = db.GIAOVIENs.Find(id);
+            if (gIAOVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gIAOVIEN);
+        }
+
+        // POST: Admin/GIAOVIENs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            GIAOVIEN gIAOVIEN = db.GIAOVIENs.Find(id);
+            db.GIAOVIENs.Remove(gIAOVIEN);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
